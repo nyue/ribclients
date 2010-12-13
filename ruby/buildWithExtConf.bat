aec4778072612e715a0c2cc67f20258bb3fbf103
yue.nicholas <yue.nicholas@8602ec3a-91f7-11de-bfab-6fa4ee52fadd>
2010-12-13 02:02:59 +0000

    Build using utils library being build with CMake

--- ruby/buildWithExtConf.bat
+++ ruby/buildWithExtConf.bat
@@ -6,10 +6,10 @@ del /f RIBClientRUBY_wrap.cxx *.def *.pdb *.obj
 swig -ruby -includeall -c++ -I..\utils -o RIBClientRUBY_wrap.cxx RIBClient.i 
 
 REM set USE_PRMAN=1
-REM ruby extconf.rb --with-rman-dir=$RMANTREE --with-utils-include=..\utils\ --with-utils-lib=..\build\
+REM ruby extconf.rb --with-rman-dir=$RMANTREE --with-utils-include=..\utils\ --with-utils-lib=..\build_nmake\utils
 
 set USE_DELIGHT=1
 set RUBY_VERSION=180
-ruby extconf.rb --with-rman-dir=%DELIGHT% --with-utils-include=..\utils\ --with-utils-lib=..\build\
+ruby extconf.rb --with-rman-dir=%DELIGHT% --with-utils-include=..\utils\ --with-utils-lib=..\build_nmake\utils
 
-nmake clean all
+nmake all
